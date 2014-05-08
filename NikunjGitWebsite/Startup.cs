7c531b60d258deee528e7bf9211dbef817c8da37
nikunjbanker <nikunj_banker@hotmail.com>
2014-05-08 14:20:40 +0530

    First commit

---
+++ NikunjGitWebsite/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NikunjGitWebsite.Startup))]
+namespace NikunjGitWebsite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
